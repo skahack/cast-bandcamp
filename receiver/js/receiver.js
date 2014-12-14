18bc6137f0735fbdc816c373feaafda295c1433d
Hiroki Sato <m@skahack.com>
2014-12-14 14:08:16 +0900

    receiver: remove global dependency

--- receiver/js/receiver.js
+++ receiver/js/receiver.js
@@ -7,23 +7,24 @@ var Root = require('./components/root');
 var namespace = 'urn:x-cast:com.skahack.cast.bandcamp';
 
 cast.receiver.logger.setLevelValue(cast.receiver.LoggerLevel.DEBUG);
-window.manager = cast.receiver.CastReceiverManager.getInstance();
+var manager = cast.receiver.CastReceiverManager.getInstance();
+var messageBus = manager.getCastMessageBus(namespace);
 debug('Starting Receiver Manager');
 
 manager.onReady = function(e) {
   debug('Received Ready event: ' + JSON.stringify(e.data));
-  window.manager.setApplicationState("Application status is ready...");
+  manager.setApplicationState("Application status is ready...");
 };
 
 manager.onSenderConnected = function(e) {
   debug('Received Sender Connected event: ' + e.data);
-  debug(window.manager.getSender(e.data));
+  debug(manager.getSender(e.data));
 };
 
 manager.onSenderDisconnected = function(e) {
   debug('Received Sender Disconnected event: ' + e.data);
   var reason = cast.receiver.system.DisconnectReason.REQUESTED_BY_SENDER;
-  if (window.manager.getSenders().length === 0 && event.reason === reason) {
+  if (manager.getSenders().length === 0 && event.reason === reason) {
     window.close();
   }
 };
@@ -32,16 +33,15 @@ manager.onSystemVolumeChanged = function(e) {
   debug('Received System Volume Changed event: ' + e.data.level + ' ' + e.data.muted);
 };
 
-window.messageBus = window.manager.getCastMessageBus(namespace);
 
-window.messageBus.onMessage = function(e) {
+messageBus.onMessage = function(e) {
   debug('Message [' + e.senderId + ']: ' + e.data);
 
   var album = new Album(JSON.parse(e.data));
   Player.init(album);
   Player.play();
 
-  window.messageBus.send(e.senderId, e.data);
+  messageBus.send(e.senderId, e.data);
 };
 
 manager.start({statusText: "Application is starting"});
